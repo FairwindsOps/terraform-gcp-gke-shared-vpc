05df9b01bb2374a3a1fa3d5377c5b61794dfd341
dosullivan <daniel@osullivan.io>
2019-10-31 11:42:22 -0500

    updating to terraform 0.12

--- create_shared_vpc.tf
+++ create_shared_vpc.tf
@@ -16,23 +16,22 @@
 ## This terraform configuration can only be ran by an account that is a Shared VPC Admin.
 ## (see https://cloud.google.com/vpc/docs/provisioning-shared-vpc#nominating_shared_vpc_admins_for_the_organization)
 
-
 #######################
 # Define all the variables we'll need
 #######################
 
 variable "host_project_name" {
-	description = "The shared VPC host project name"
+  description = "The shared VPC host project name"
 }
 
 variable "service_projects" {
   description = "map of service project names to project IDs"
-  type        = "map"
+  type        = map(string)
 }
 
 variable "service_networks" {
   description = "map of service project names to associated subnetworks"
-  type        = "map"
+  type        = map(string)
 }
 
 #######################
@@ -42,12 +41,12 @@ variable "service_networks" {
 #######################
 
 resource "google_project_service" "host-compute" {
-  project  = "${var.host_project_name}"
+  project = var.host_project_name
   service = "compute.googleapis.com"
 }
 
 resource "google_project_service" "host-container" {
-  project  = "${var.host_project_name}"
+  project = var.host_project_name
   service = "container.googleapis.com"
 }
 
@@ -55,49 +54,47 @@ resource "google_project_service" "host-container" {
 # enable shared VPC for host project, attach service projects to host project
 #######################
 resource "google_compute_shared_vpc_host_project" "host-shared-vpc" {
-  project = "${var.host_project_name}"
+  project = var.host_project_name
 }
 
 resource "google_compute_shared_vpc_service_project" "service" {
-  count           = "${length(var.service_projects)}"
-  host_project    = "${var.host_project_name}"
-  service_project = "${element(keys(var.service_projects), count.index)}"
+  count           = length(var.service_projects)
+  host_project    = var.host_project_name
+  service_project = element(keys(var.service_projects), count.index)
 
   // Shared VPC needs to be enabled on host project fist
-  depends_on = ["google_compute_shared_vpc_host_project.host-shared-vpc"]
+  depends_on = [google_compute_shared_vpc_host_project.host-shared-vpc]
 }
 
 #######################
 # set necessary network permissions on host network for service accounts from service projects, so that they can create resources within their given subnetworks of the host network
 #######################
 resource "google_compute_subnetwork_iam_member" "cloudservices" {
-  count      = "${length(var.service_projects)}"
-  subnetwork = "${lookup(var.service_networks, element(keys(var.service_networks), count.index))}"
+  count      = length(var.service_projects)
+  subnetwork = var.service_networks[element(keys(var.service_networks), count.index)]
   role       = "roles/compute.networkUser"
-  member     = "serviceAccount:${lookup(var.service_projects, element(keys(var.service_projects), count.index))}@cloudservices.gserviceaccount.com"
+  member     = "serviceAccount:${var.service_projects[element(keys(var.service_projects), count.index)]}@cloudservices.gserviceaccount.com"
 }
 
 resource "google_compute_subnetwork_iam_member" "container-engine-robot" {
-  count      = "${length(var.service_projects)}"
-  subnetwork = "${lookup(var.service_networks, element(keys(var.service_networks), count.index))}"
+  count      = length(var.service_projects)
+  subnetwork = var.service_networks[element(keys(var.service_networks), count.index)]
   role       = "roles/compute.networkUser"
-  member     = "serviceAccount:service-${lookup(var.service_projects, element(keys(var.service_projects), count.index))}@container-engine-robot.iam.gserviceaccount.com"
+  member     = "serviceAccount:service-${var.service_projects[element(keys(var.service_projects), count.index)]}@container-engine-robot.iam.gserviceaccount.com"
 }
-
 
 #######################
 # enable GKE API for service projects
 #######################
 resource "google_project_service" "service-container" {
-  count   = "${length(var.service_projects)}"
-  project = "${element(keys(var.service_projects), count.index)}"
+  count   = length(var.service_projects)
+  project = element(keys(var.service_projects), count.index)
   service = "container.googleapis.com"
 }
 
-
-resource "google_project_iam_member" "gke-service-account"{
-  count   = "${length(var.service_projects)}"
-  project = "${var.host_project_name}"
-  member  = "serviceAccount:service-${lookup(var.service_projects, element(keys(var.service_projects), count.index))}@container-engine-robot.iam.gserviceaccount.com"
+resource "google_project_iam_member" "gke-service-account" {
+  count   = length(var.service_projects)
+  project = var.host_project_name
+  member  = "serviceAccount:service-${var.service_projects[element(keys(var.service_projects), count.index)]}@container-engine-robot.iam.gserviceaccount.com"
   role    = "roles/container.hostServiceAgentUser"
 }
